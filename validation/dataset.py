d72d636cf58dbdc8cb1677bfaa921aaaf0d6bf75
VaidehiBhat <142377329+VaidehiBhat@users.noreply.github.com>
2025-02-18 03:36:45 +0530

    Add files via upload

---
+++ validation/dataset.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset
+
+class CustomDataset(Dataset):
+    def __init__(self, subset, transform=None):
+        self.subset = subset
+        self.transform = transform
+
+    def __getitem__(self, index):
+        data, target = self.subset[index]
+        if self.transform:
+            data = self.transform(data)
+        return data, target
+
+    def __len__(self):
+        return len(self.subset)
